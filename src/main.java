6b13d6ca17c57aa9072bccdcc120c1e74606e9ec
Nycollas Eufrasio da Silva <nycollas.silva-ext@viavarejo.com.br>
2021-03-18 21:56:23 -0300

    Inicial

---
+++ src/main.java
@@ -0,0 +1,54 @@
+
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class main {
+
+	/*
+	public static void main(String args[]) {
+		System.out.println("OK");
+		
+		try {
+			ServerSocket server = new ServerSocket(3322);
+			InetAddress inet = server.getInetAddress();
+			
+			System.out.println("HostAddress: "+inet.getHostAddress());
+			System.out.println("HostName: "+inet.getHostName());
+			
+		} catch (Exception e) {
+			
+		}
+		
+		
+		
+	}*/
+	
+	public static void main(String[] args) {
+	    try {
+	      // Instancia o ServerSocket ouvindo a porta 12345
+	      ServerSocket servidor = new ServerSocket(12345);
+	      System.out.println("Servidor ouvindo a porta 12345");
+	      while(true) {
+	        // o método accept() bloqueia a execução até que
+	        // o servidor receba um pedido de conexão
+	        Socket cliente = servidor.accept();
+	        System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+	        ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+	        saida.flush();
+	        saida.writeObject(new Date());
+	        saida.close();
+	        cliente.close();
+	      }
+	    }
+	    catch(Exception e) {
+	       System.out.println("Erro: " + e.getMessage());
+	    }
+	    finally {
+	    	
+	    }
+	  }
+	
+}
